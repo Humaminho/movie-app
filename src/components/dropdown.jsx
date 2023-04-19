8698a61d8d2ebca24d9ef51b2dab848b46a6507d
humam <hu666mam@gmail.com>
2023-04-19 03:14:31 +0000

    search bar suggestions and minor fixes

---
+++ src/components/dropdown.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles/dropdown.css'
+
+export default function DropDown({list}) {
+  return (
+    <div className="drop-down">
+      hello
+    </div>
+  )
+}
---
+++ src/components/infoSection.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export default function InfoSection({movie}) {
+  return (
+		<div className="info-section">
+			<img
+				className="movie-photo"
+				src={
+					movie.poster_path
+						? 'https://image.tmdb.org/t/p/w500' + movie.poster_path
+						: './src/assets/notfound.png'
+				}
+				alt={movie && movie.title + ' poster picture'}
+				height="480px"
+				width="300px"
+			/>
+			<div className="movie-info">
+				<p className="movie-title">
+					{movie && movie.title && movie.title.toUpperCase()}
+				</p>
+				<div className="text-section">
+					<p className="blue">Release date:</p>
+					<p className="big white">
+						{movie && movie.release_date
+							? movie.release_date.replace(/-/g, '/')
+							: 'Not found :('}
+					</p>
+				</div>
+				<div className="text-section">
+					<p className="blue">Description:</p>
+					<p className="description big white">
+						{movie && movie.overview}
+					</p>
+				</div>
+				<div className="text-section">
+					<p className="blue">Popularity:</p>
+					<p className="big white">
+						{movie && Math.floor(movie.popularity) + ' %'}
+					</p>
+				</div>
+			</div>
+		</div>
+  );
+}
